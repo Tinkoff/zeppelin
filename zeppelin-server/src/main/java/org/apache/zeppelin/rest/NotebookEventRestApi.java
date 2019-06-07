791d4dc28c9afe846a723eb4e9aa23d68209b4dd
George Klimov <klim.electronicmail@gmail.com>
2019-06-07 18:52:07 +0300

    [ZP-105] update UI and REST API

--- zeppelin-server/src/main/java/org/apache/zeppelin/rest/NotebookEventRestApi.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/rest/NotebookEventRestApi.java
@@ -1,15 +1,24 @@
 package org.apache.zeppelin.rest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.zeppelin.realm.AuthenticationInfo;
-import org.apache.zeppelin.realm.AuthorizationService;
 import org.apache.zeppelin.rest.message.JsonResponse;
 import org.apache.zeppelin.websocket.ConnectionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.tinkoff.zeppelin.core.notebook.Note;
 import ru.tinkoff.zeppelin.core.notebook.NoteEvent;
+import ru.tinkoff.zeppelin.core.notebook.NoteEvent.Notification;
+import ru.tinkoff.zeppelin.core.notebook.NoteEvent.Type;
 import ru.tinkoff.zeppelin.engine.NoteService;
 import ru.tinkoff.zeppelin.storage.NoteDAO;
 import ru.tinkoff.zeppelin.storage.NoteEventDAO;
@@ -34,30 +43,60 @@ public class NotebookEventRestApi extends AbstractRestApi {
         this.noteDAO = noteDAO;
     }
 
+    /**
+     * Returns note notification configuration.
+     * eventType -> {notificationType -> userlist}.
+     *
+     * @param noteId
+     * @return
+     */
+    @GetMapping(value = "/{noteId}/subscriptions", produces = "application/json")
+    public ResponseEntity getNoteSubscriptions(@PathVariable("noteId") final long noteId) {
+        final HashMap<String, HashMap<String, List<String>>> subscriptions = new HashMap<>(Type.values().length);
+
+        for (final Type eventType : NoteEvent.Type.values()) {
+            final HashMap<String, List<String>> currentEventNotifications = new HashMap<>(Notification.values().length);
+
+            for (final Notification notificationType : Notification.values()) {
+                currentEventNotifications.put(notificationType.name(), new ArrayList<>());
+            }
+            noteEventDAO.getNoteEvent(noteId, eventType.name())
+                .forEach(s -> currentEventNotifications.get(s.getNotification().name()).add(s.getName()));
+
+            subscriptions.put(eventType.name(), currentEventNotifications);
+        }
+
+        return new JsonResponse(HttpStatus.OK, subscriptions).build();
+    }
 
     @PostMapping(value = "/{noteId}", produces = "application/json")
     public ResponseEntity noteSubscription(
             @PathVariable("noteId") final Long noteId,
             @RequestParam("type") final String eventType,
             @RequestParam("action") final String eventAction,
-            @RequestParam("notification") final String eventNotification) {
+            @RequestParam("notification") final String eventNotification,
+            @RequestParam("user") final String user) {
+        final Note note = noteDAO.get(noteId);
 
-        final AuthenticationInfo authenticationInfo = AuthorizationService.getAuthenticationInfo();
+        if (note == null || schedulerDAO.getByNote(noteId) == null){
+            return new JsonResponse(HttpStatus.BAD_REQUEST, "Scheduler not exist.").build();
+        }
 
-        if (noteDAO.get(noteId) == null || schedulerDAO.getByNote(noteId) == null){
-            return new JsonResponse(HttpStatus.BAD_REQUEST, "Scheduler or note not exist.").build();
+        if (!userHasReaderPermission(note)) {
+            return new JsonResponse(HttpStatus.UNAUTHORIZED, "Insufficient privileges, you must be a reader of this note").build();
         }
 
         if (StringUtils.isEmpty(eventType) || !NoteEvent.Type.containsName(eventType)) {
             return new JsonResponse(HttpStatus.BAD_REQUEST, "Event type not correct").build();
         }
 
-        if (StringUtils.isEmpty(eventAction) || !NoteEvent.Notification.containsName(eventNotification)) {
-            return new JsonResponse(HttpStatus.BAD_REQUEST, "Event action not correct").build();
+        if (StringUtils.isEmpty(eventNotification) || !NoteEvent.Notification.containsName(eventNotification)) {
+            return new JsonResponse(HttpStatus.BAD_REQUEST, "Notification type not correct").build();
         }
 
 
-        final NoteEvent noteEvent = new NoteEvent(authenticationInfo.getUser(),
+        final NoteEvent noteEvent = new NoteEvent(
+                user,
                 noteId,
                 NoteEvent.Type.valueOf(eventType.toUpperCase()),
                 NoteEvent.Notification.valueOf(eventNotification.toUpperCase()),
